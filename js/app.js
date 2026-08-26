b1b6f07457df74e0b7bfb15ae5b8177c4103759c
Marcio Sa <marcio_fsn@outlook.com>
2026-08-26 14:41:52 -0300

    Variáveis de tipos de dados

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Olá Javascript!')
+
+var userName = 'Márcio Sá'
+
+document.getElementById('user-name').innerHTML = userName
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
